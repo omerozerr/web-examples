bf6021005feb4a8d9f8f43f365de06ef906280e7
omerozerr <omer55-7@hotmail.com>
2024-07-13 19:58:02 +0200

    main page

--- dapps/web3modal/next/src/app/page.tsx
+++ dapps/web3modal/next/src/app/page.tsx
@@ -1,15 +1,39 @@
 "use client";
-import Connect from "@/components/Connect";
 import styles from "./page.module.css";
-import { useRouter } from "next/navigation"; // Update import to next/navigation
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function Home() {
     const router = useRouter();
 
     return (
-        <div>
-            <h1>Welcome to the Threer</h1>
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <div className={styles.textSection}>
+                    <h1 className={styles.header}>Welcome to Threer</h1>
+                    <p className={styles.paragraph}>
+                        Welcome to Threer, the ultimate platform for connecting
+                        clients with top developers. Here, clients can register,
+                        browse available job offerings, and hire developers for
+                        their projects. Developers can create profiles
+                        showcasing their skills, register their services, and
+                        list job offerings. Clients can view developer profiles,
+                        including builder scores and project details, and track
+                        their purchased offerings. Both clients and developers
+                        can update their profiles with relevant information. Get
+                        started by connecting your wallet and navigating through
+                        our intuitive interface to explore and manage job
+                        offerings efficiently.
+                    </p>
+                </div>
+                <div className={styles.logoSection}>
+                    <img
+                        src="/DevChainBg.png"
+                        alt="Logo"
+                        className={styles.logo}
+                    />
+                </div>
+            </div>
         </div>
     );
 }
